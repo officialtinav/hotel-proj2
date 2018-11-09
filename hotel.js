9ba48a08e7c0ebc1f91f8b4ffe0d644cd7eb6590
tina <34047417+officialtinav@users.noreply.github.com>
2018-11-09 12:16:38 -1000

    Add files via upload

--- hotel.js
+++ hotel.js
@@ -15,77 +15,125 @@ var arrGuest5 = [];
 var arrName6 = [];
 var arrGuest6 = [];
 
+function lockRoom1() {
+  document.getElementById('room1CheckIn').style.visibility = "hidden";
+}
+
+function unlockRoom1() {
+  document.getElementById('room1CheckIn').style.visibility = "visible";
+}
+
+function lockRoom2() {
+  document.getElementById('room2CheckIn').style.visibility = "hidden";
+}
+
+function unlockRoom2() {
+  document.getElementById('room2CheckIn').style.visibility = "visible";
+}
+function lockRoom3() {
+  document.getElementById('room3CheckIn').style.visibility = "hidden";
+}
+
+function unlockRoom3() {
+  document.getElementById('room3CheckIn').style.visibility = "visible";
+}
+
+function lockRoom4() {
+  document.getElementById('room4CheckIn').style.visibility = "hidden";
+}
+
+function unlockRoom4() {
+  document.getElementById('room4CheckIn').style.visibility = "visible";
+}
+
+function lockRoom5() {
+  document.getElementById('room5CheckIn').style.visibility = "hidden";
+}
+
+function unlockRoom5() {
+  document.getElementById('room5CheckIn').style.visibility = "visible";
+}
+
+function lockRoom6() {
+  document.getElementById('room6CheckIn').style.visibility = "hidden";
+}
+
+function unlockRoom6() {
+  document.getElementById('room6CheckIn').style.visibility = "visible";
+}
+
  //timer to kick out guests for cleaning
   
   function timeOut() {
     setTimeout (function() {
-      //make a variable for button
-      var button = document.getElementsByTagName('button')[0];
-      var parent = button.parentNode;
-      //remove check-in button 1
-      parent.removeChild(button);
+      document.getElementsByTagName('button')[0].style.visibility = "hidden";
       document.getElementById('cleanUp').innerHTML = "Come back another time, room is being cleaned!";
-      //1000 milliseconds in one second = 15000 is 15 seconds
-    },15000);
+      //1000 milliseconds in one second = 5000 is 15 seconds
+    },5000);
   }
-  
-  
-    function timeOut2() {
+  // MR KAMS DEMO CODE. *******************************//
+  function timeOut2() {
+    setTimeout (function() {
+      document.getElementsByTagName('button')[1].style.visibility = "hidden";
+      document.getElementById('cleanUp2').innerHTML = "Come back another time, room is being cleaned!";
+       //1000 milliseconds in one second = 5000 is 15 seconds
+    },5000);
+  }
+
+  //END ************************************************//
+
+
+   /*function timeOut2() {
     setTimeout (function() {
       var button = document.getElementsByTagName('button')[1];
       var parent = button.parentNode;
         //remove check-in button 3
       parent.removeChild(button);
       document.getElementById('cleanUp2').innerHTML = "Come back another time, room is being cleaned!";
-       //1000 milliseconds in one second = 15000 is 15 seconds
-    },15000);
-  }
-  
+       //1000 milliseconds in one second = 5000 is 15 seconds
+    },5000);
+  }*/
+ 
     function timeOut3() {
     setTimeout (function() {
-      var button = document.getElementsByTagName('button')[2];
-      var parent = button.parentNode;
-        //remove check-in button 3
-      parent.removeChild(button);
+      document.getElementsByTagName('button')[2].style.visibility = "hidden";
       document.getElementById('cleanUp3').innerHTML = "Come back another time, room is being cleaned!";
-       //1000 milliseconds in one second = 15000 is 15 seconds
-    },15000);
+       //1000 milliseconds in one second = 5000 is 15 seconds
+    },5000);
   }
   
     function timeOut4() {
     setTimeout (function() {
-      var button = document.getElementsByTagName('button')[3];
-      var parent = button.parentNode;
-        //remove check-in button 4
-      parent.removeChild(button);
+      document.getElementsByTagName('button')[3].style.visibility = "hidden";
       document.getElementById('cleanUp4').innerHTML = "Come back another time, room is being cleaned!";
-       //1000 milliseconds in one second = 15000 is 15 seconds
-    },15000);
+       //1000 milliseconds in one second = 5000 is 15 seconds
+    },5000);
   }
   
     function timeOut5() {
     setTimeout (function() {
-      var button = document.getElementsByTagName('button')[4];
-      var parent = button.parentNode;
-        //remove check-in button 5
-      parent.removeChild(button);
+      document.getElementsByTagName('button')[4].style.visibility = "hidden";
       document.getElementById('cleanUp5').innerHTML = "Come back another time, room is being cleaned!";
-       //1000 milliseconds in one second = 15000 is 15 seconds
-    },15000);
+       //1000 milliseconds in one second = 5000 is 15 seconds
+    },5000);
   }
   
     function timeOut6() {
     setTimeout (function() {
-      var button = document.getElementsByTagName('button')[5];
-      var parent = button.parentNode;
-        //remove check-in button 6
-      parent.removeChild(button);
+      document.getElementsByTagName('button')[5].style.visibility = "hidden";
       document.getElementById('cleanUp6').innerHTML = "Come back another time, room is being cleaned!";
-       //1000 milliseconds in one second = 15000 is 15 seconds
-    },15000);
+       //1000 milliseconds in one second = 5000 is 15 seconds
+    },5000);
   }
+ 
+//*****************************ADD GUESTS  **************************** */
+
+
+
+
 
 function addName() {
+ // timeOut();
   //get name from guest
   var name = document.getElementById('name').value;
  //clear the input box by putting quotation
@@ -116,6 +164,7 @@ else {
 }}
   
 function addName2(){
+// timeOut2();
   //get name from guest
   var name = document.getElementById('name2').value;
  //clear the input box by putting quotation
@@ -123,9 +172,9 @@ function addName2(){
  //unshift the name to arrName
  document.getElementsByTagName('h3')[1].innerHTML = "Room 102";
   
-if (arrName3.length < 4) {
-   arrName3.unshift(name);
-   arrGuest3.unshift(name);
+if (arrName2.length < 4) {
+   arrName2.unshift(name);
+   arrGuest2.unshift(name);
   //create element for li
   var li = document.createElement('li');
   //take entered name and stores it as liText
@@ -138,7 +187,7 @@ if (arrName3.length < 4) {
   var ol = document.getElementById('list2');
   //append li to ol
   ol.appendChild(li);
-  document.getElementById('allGuest2').innerHTML = "All Guests:" + arrGuest3; }
+  document.getElementById('allGuest2').innerHTML = "All Guests:" + arrGuest2; }
 
 else {//if name is inserted more than four times, do this
       document.getElementsByTagName('h3')[0].innerHTML = "Room is full!";
@@ -150,6 +199,7 @@ else {//if name is inserted more than four times, do this
 }
 
 function addName3(){
+ // timeOut3();
   //get name from guest
   var name = document.getElementById('name3').value;
  //clear the input box by putting quotation
@@ -183,6 +233,7 @@ else {//if name is inserted more than four times, do this
  
 }
 function addName4(){
+  //timeOut4();
   //get name from guest
   var name = document.getElementById('name4').value;
  //clear the input box by putting quotation
@@ -209,13 +260,14 @@ if (arrName4.length < 4) {
 
 else {//if name is inserted more than four times, do this
       document.getElementsByTagName('h3')[0].innerHTML = "Room is full!";
-     
+      
  
 }
   
  
 }
 function addName5(){
+  //timeOut5();
   //get name from guest
   var name = document.getElementById('name5').value;
  //clear the input box by putting quotation
@@ -240,23 +292,24 @@ if (arrName5.length < 4) {
   ol.appendChild(li);
   document.getElementById('allGuest5').innerHTML = "All Guests:" + arrGuest5; }
 
-else{//if name is inserted more than four times, do this
+else {//if name is inserted more than four times, do this
       document.getElementsByTagName('h3')[0].innerHTML = "Room is full!";
-     
+      
  
 }
   
  
 }
 function addName6(){
+  //timeOut6();
   //get name from guest
   var name = document.getElementById('name6').value;
-//clear the input box by putting quotation
+ //clear the input box by putting quotation
   document.getElementById('name6').value = "";
  //unshift the name to arrName
  document.getElementsByTagName('h3')[5].innerHTML = "Room 106";
   
-if (arrGuest6.length < 4) {
+if (arrName6.length < 4) {
    arrName6.unshift(name);
    arrGuest6.unshift(name);
   //create element for li
@@ -273,9 +326,9 @@ if (arrGuest6.length < 4) {
   ol.appendChild(li);
   document.getElementById('allGuest6').innerHTML = "All Guests:" + arrGuest6; }
 
-else{//if name is inserted more than four times, do this
+else {//if name is inserted more than four times, do this
       document.getElementsByTagName('h3')[0].innerHTML = "Room is full!";
-      return;
+      
  
 }
   
@@ -296,13 +349,12 @@ function deleteAll(){ //remove all listed items by putting 0
 }
 
 function deleteAll2(){ //remove all listed items by putting 0
-  arrName2.length = 0;
-  document.getElementsByTagName('h3')[1].innerHTML = "All Guests Removed";
-   var ol = document.getElementById('list2');
-   while (ol.hasChildNodes()){ //as long as <ol> has a child, remove all child
-     ol.removeChild(ol.firstChild);
-   
-     }
+    arrName2.length = 0;
+    document.getElementsByTagName('h3')[1].innerHTML = "All Guests Removed";
+     var ol = document.getElementById('list2');
+     while (ol.hasChildNodes()){ //as long as <ol> has a child, remove all child
+       ol.removeChild(ol.firstChild);
+       }
   
 }
 function deleteAll3(){ //remove all listed items by putting 0
@@ -315,30 +367,31 @@ function deleteAll3(){ //remove all listed items by putting 0
   
 }
 function deleteAll4(){ //remove all listed items by putting 0
- arrName4.length = 0;
-  document.getElementsByTagName('h3')[3].innerHTML = "All Guests Removed";
-   var ol = document.getElementById('list4');
-   while (ol.hasChildNodes()){ //as long as <ol> has a child, remove all child
-     ol.removeChild(ol.firstChild);
-     
-    
-}
+    arrName4.length = 0;
+    document.getElementsByTagName('h3')[3].innerHTML = "All Guests Removed";
+     var ol = document.getElementById('list4');
+     while (ol.hasChildNodes()){ //as long as <ol> has a child, remove all child
+       ol.removeChild(ol.firstChild);
+       }
+  
 }
 function deleteAll5(){ //remove all listed items by putting 0
-  arrName5.length = 0;
-  document.getElementsByTagName('h3')[4].innerHTML = "All Guests Removed";
-   var ol = document.getElementById('list5');
-   while (ol.hasChildNodes()){ //as long as <ol> has a child, remove all child
-     ol.removeChild(ol.firstChild);
-     }
+    arrName5.length = 0;
+    document.getElementsByTagName('h3')[4].innerHTML = "All Guests Removed";
+     var ol = document.getElementById('list5');
+     while (ol.hasChildNodes()){ //as long as <ol> has a child, remove all child
+       ol.removeChild(ol.firstChild);
+       }
+  
 }
 function deleteAll6(){ //remove all listed items by putting 0
-  arrName6.length = 0;
-  document.getElementsByTagName('h3')[5].innerHTML = "All Guests Removed";
-   var ol = document.getElementById('list6');
-   while (ol.hasChildNodes()){ //as long as <ol> has a child, remove all child
-     ol.removeChild(ol.firstChild);
-  }
+    arrName6.length = 0;
+    document.getElementsByTagName('h3')[5].innerHTML = "All Guests Removed";
+     var ol = document.getElementById('list6');
+     while (ol.hasChildNodes()){ //as long as <ol> has a child, remove all child
+       ol.removeChild(ol.firstChild);
+       }
+  
 }
 function removeName() { //remove the listed name
   arrName.length--;
@@ -386,4 +439,3 @@ function removeName6() { //remove the listed name
   var li = ol.childNodes[0]; //define which li to remove
   ol.removeChild(li)[0];
  }
- 
